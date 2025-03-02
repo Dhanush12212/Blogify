bd57fef573ad29916ce5e2168c39bb1dadbcf11e
Dhanaush12212 <dhanush12212@gmail.com>
2025-03-02 18:14:28 +0530

    Changed DB connection

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import AuthRoute from './routes/AuthRoute.js';
 import  BlogRoute from './routes/BlogRoute.js';
-import connectDB from './db/connectDB.js'; 
+// import connectDB from './db/connectDB.js'; 
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 const app = express();
@@ -13,8 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", 
-    credentials: true  
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    credentials: true
 }));
 
 const PORT = process.env.PORT || 8000;
@@ -24,25 +24,49 @@ app.use('/api/Auth',AuthRoute);
 app.use('/api/BlogHome', BlogRoute);
  
 
-const startServer = async() => {
-    try {
-        await connectDB();
-        app.listen(PORT, "0.0.0.0", () => {
-            console.log(
-                JSON.stringify({ message: `Server started on port ${PORT}`, status: "success"})
-            );
-        });
-    }   
-    catch(error) {
-        console.error(
-            JSON.stringify({ 
-                message: "Server failed to start", 
-                status: "error", 
-                error: error.message, 
-            })
-        );
-        process.exit(1);
-    }
-};
+// const startServer = async() => {
+//     try {
+//         await connectDB();
+//         app.listen(PORT, "0.0.0.0", () => {
+//             console.log(
+//                 JSON.stringify({ message: `Server started on port ${PORT}`, status: "success"})
+//             );
+//         });
+//     }   
+//     catch(error) {
+//         console.error(
+//             JSON.stringify({ 
+//                 message: "Server failed to start", 
+//                 status: "error", 
+//                 error: error.message, 
+//             })
+//         );
+//         process.exit(1);
+//     }
+// };
 
-startServer();
+// startServer();
+
+
+const connectDB = () => {
+    mongoose.set("strictQuery", true);
+    mongoose
+      .connect(process.env.MONGODB_URL)
+      .then(() => console.log("Connected to Mongo DB"))
+      .catch((err) => {
+        console.error("failed to connect with mongo");
+        console.error(err);
+      });
+  };
+  
+  const startServer = async () => {
+    try {
+      connectDB();
+      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  
+  startServer();
