d9794282a6164ae2215c92a39cec53654446539c
Dhanaush12212 <dhanush12212@gmail.com>
2025-03-02 19:07:02 +0530

    Final Commit

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173" ,"https://blogify-eight-pi.vercel.app/"],  
+    origin: ["http://localhost:5173" ,"https://blogify-eight-pi.vercel.app"],  
     credentials: true  
 }));
 
